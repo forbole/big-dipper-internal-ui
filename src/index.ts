bd48b230e22323b65374390fae240dae9dc4a5c1
Wingman <ryuash123@gmail.com>
2020-10-12 10:30:44 +0800

    wip: add icons

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
+import {
+  TelegramIcon,
+  MediumIcon,
+  FacebookIcon,
+  ForboleIcon,
+  GithubIcon,
+  TwitterIcon,
+  YoutubeIcon,
+} from '@icons';
 import ButtonExample from './button_example';
 import DataBlock from './data_block';
 
 export {
   ButtonExample,
   DataBlock,
+  TelegramIcon,
+  MediumIcon,
+  FacebookIcon,
+  ForboleIcon,
+  GithubIcon,
+  TwitterIcon,
+  YoutubeIcon,
 };
