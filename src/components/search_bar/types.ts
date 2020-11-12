8b2dd969e6c630f8d18ea90e2945f60c74a0f1eb
Wingman <ryuash123@gmail.com>
2020-11-12 23:53:34 +0800

    .

--- src/components/search_bar/types.ts
+++ src/components/search_bar/types.ts
@@ -1,7 +1,7 @@
 import {
   MobileProps,
   searchBar,
-} from '../mobile_nav/types';
+} from '../mobile_navbar/types';
 
 export interface SearchBarProps extends MobileProps {
   searchBar: searchBar;
